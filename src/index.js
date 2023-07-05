3283ebe5d909cf0833163fedf0397ebfcf6ced76
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-05 09:53:23 -0700

    WIP showing wrong guesses

--- src/index.js
+++ src/index.js
@@ -3,30 +3,132 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import './hangman.js';
 
-for (let i=0;i<26;i++){
-  const element = document.querySelector(`#letter${i}`);
-  element.addEventListener("click", getLetter);
+const game = {
+  secretWords: ["ALESANDRIA", "ALEJANDRA", "AARON", "JASE", "PIER"],
+  secretWord: "",
+  placeholders: [],
+  guesses: 6,
+
+  newGame() {
+    const randomIndex = Math.floor(Math.random() * this.secretWords.length);
+    this.secretWord = this.secretWords[randomIndex];
+    this.placeholders = Array(this.secretWord.length).fill("_");
+    this.guesses = 6;
+    console.log("Game");
+  },
+
+  checkLetter(letter) {
+    const letterFound = this.secretWord.includes(letter);
+    if (!letterFound) {
+      this.guesses--;
+    } else {
+      for (let i = 0; i < this.secretWord.length; i++) {
+        if (this.secretWord[i] === letter) {
+          this.placeholders[i] = letter;
+        }
+      }
+    }
+    return letterFound;
+  },
+
+  isWordComplete() {
+    return !this.placeholders.includes("_");
+  },
+};
+
+
+// ui logic 
+
+const startGameButton = document.getElementById("startGame");
+const reloadGameButton = document.getElementById("reloadGame");
+const wordDisplay = document.getElementById("wordDisplay");
+const letters = document.querySelectorAll(".letter");
+console.log(letters)
+const winningMessage = document.getElementById("winningMessage");
+const guessCount = document.getElementById("guessCount");
+
+startGameButton.addEventListener("click", startGame);
+console.log('startGame', startGame)
+reloadGameButton.addEventListener("click", reloadGame);
+letters.forEach((letter) => {
+  letter.addEventListener("click", handleLetterClick);
+  console.log('letter')
+});
+
+function startGame() {
+  game.newGame();
+  updateWordDisplay();
+  disableElement(startGameButton);
+  enableElement(reloadGameButton);
+  enableLetters();
+  hideMessage();
+  updateGuessCount();
 }
 
-function getLetter(e){
-  const input = this.innerHTML;
-  this.style.visibility='hidden';
-  
-}
+function handleLetterClick(event) {
+  if (game.guesses === 0) return;
+  console.log("handle")
+  const letter = event.target.innerText;
+  const letterFound = game.checkLetter(letter);
+  updateWordDisplay();
+  disableElement(event.target);
 
-//showing images based off of attempts left.
-if (attempts === 6) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png');
-  } else if (attempts === 5) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/3/30/Hangman-1.png');
-  } else if (attempts === 4) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/7/70/Hangman-2.png');
-  } else if (attempts === 3) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/9/97/Hangman-3.png');
-  } else if (attempts === 2) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/2/27/Hangman-4.png');
-  } else if (attempts === 1) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Hangman-5.png');
-  } else if (attempts === 0) {
-  hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png');
+  if (letterFound && game.isWordComplete()) {
+    showWinningMessage("Congratulations! You guessed the word.");
+    enableElement(reloadGameButton);
+    disableLetters();
+  } else if (!letterFound && game.guesses === 0) {
+    showWinningMessage("Game Over! You ran out of guesses.");
+    enableElement(reloadGameButton);
+    disableLetters();
   }
+  updateGuessCount();
+}
+
+function reloadGame() {
+  location.reload();
+}
+
+function updateWordDisplay() {
+  wordDisplay.textContent = game.placeholders.join(" ");
+}
+
+function disableElement(element) {
+  element.disabled = true;
+}
+
+function enableElement(element) {
+  element.disabled = false;
+}
+
+function enableLetters() {
+  letters.forEach((letter) => {
+    enableElement(letter);
+    letter.classList.remove("chosen");
+  });
+}
+
+function disableLetters() {
+  letters.forEach((letter) => {
+    disableElement(letter);
+  });
+}
+
+function showWinningMessage(text) {
+  winningMessage.textContent = text;
+  winningMessage.style.display = "block";
+  setTimeout(reloadGame, 10000);
+}
+
+function hideMessage() {
+  winningMessage.style.display = "none";
+}
+
+function updateGuessCount() {
+  guessCount.textContent = game.guesses;
+}
+
+letters.forEach((letter) => {
+  letter.addEventListener("click", handleLetterClick);
+  letter.style.cursor = "pointer";
+});
