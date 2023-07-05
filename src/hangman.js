8dbe8126567385735942e00293448fdfd24dc39c
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-05 16:45:47 -0700

    Hangman is working, UI and BL are in seperate JS.

--- src/hangman.js
+++ src/hangman.js
@@ -1,31 +1,32 @@
-export class (`insert class name here`){
-const game = {
-    secretWords: ['pluto', 'mars', 'jupiter', 'earth', 'mercury', 'venus', 'uranus', 'neptune', 'saturn'],
-    secretWord: "",
-    placeholders: [],
-    guesses: 6,
-    newGame() {
-      const randomIndex = Math.floor(Math.random() * this.secretWords.length);
-      this.secretWord = this.secretWords[randomIndex];
-      this.placeholders = Array(this.secretWord.length).fill("_");
-      this.guesses = 6;
-      console.log("Game");
-    },
-    checkLetter(letter) {
-      const letterFound = this.secretWord.includes(letter);
-      if (!letterFound) {
-        this.guesses--;
-      } else {
-        for (let i = 0; i < this.secretWord.length; i++) {
-          if (this.secretWord[i] === letter) {
-            this.placeholders[i] = letter;
-          }
+export class Hangman {
+  constructor () {
+    this.secretWords = ['pluto', 'mars', 'jupiter', 'earth', 'mercury', 'venus', 'uranus', 'neptune', 'saturn', 'moon', 'rover', 'space station', 'galaxy', 'star'];
+    this.secretWord = "";
+    this.placeholders = [];
+    this.guesses = 6;
+  }
+  newGame() {
+    const randomIndex = Math.floor(Math.random() * this.secretWords.length);
+    this.secretWord = this.secretWords[randomIndex];
+    this.placeholders = Array(this.secretWord.length).fill("_");
+    this.guesses = 6;
+    console.log("Game");
+  }
+  checkLetter(letter) {
+    const letterFound = this.secretWord.includes(letter.toLowerCase());
+    if (!letterFound) {
+      this.guesses--;
+    } else {
+      for (let i = 0; i < this.secretWord.length; i++) {
+        if (this.secretWord[i] === letter.toLowerCase()) {
+          this.placeholders[i] = letter;
         }
       }
-      return letterFound;
-    },
-    isWordComplete() {
-      return !this.placeholders.includes("_");
-    2},
-  };
+    }
+    return letterFound;
+  }
+
+  isWordComplete() {
+    return !this.placeholders.includes("_");
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './hangman.js';
-// ui logic 
+import { Hangman } from './hangman.js';
+
+const game = new Hangman();
 
 const startGameButton = document.getElementById("startGame");
 const reloadGameButton = document.getElementById("reloadGame");
+const wordDisplay = document.getElementById("wordDisplay");
 const letters = document.querySelectorAll(".letter");
-console.log(letters);
 const winningMessage = document.getElementById("winningMessage");
 const guessCount = document.getElementById("guessCount");
 
@@ -17,70 +16,43 @@ letters.forEach((letter) => {
   letter.addEventListener("click", handleLetterClick);
 });
 
-async function startGame() {
-  try {
-    await game.newGame();
-    updateWordDisplay();
-    disableElement(startGameButton);
-    enableElement(reloadGameButton);
-    enableLetters();
-    hideMessage();
-    updateGuessCount();
-  } catch (error) {
-    console.error("An error occurred while starting the game:", error);
-  }
-}
-function updateHangmanImage() {
-  const hangmanImg = document.getElementById("hangmanImg");
-
-  if (guesses === 6) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png');
-  } else if (guesses === 5) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/3/30/Hangman-1.png');
-  } else if (guesses === 4) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/7/70/Hangman-2.png');
-  } else if (guesses === 3) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/9/97/Hangman-3.png');
-  } else if (guesses === 2) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/2/27/Hangman-4.png');
-  } else if (guesses === 1) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Hangman-5.png');
-  } else if (guesses === 0) {
-    hangmanImg.setAttribute("src", 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png');
-  }
-  updateGuessCount()
-
+function startGame() {
+  game.newGame();
+  console.log(game);
+  updateWordDisplay();
+  disableElement(startGameButton);
+  enableElement(reloadGameButton);
+  enableLetters();
+  hideMessage();
+  updateGuessCount();
 }
 
 function handleLetterClick(event) {
   if (game.guesses === 0) return;
   const letter = event.target.innerText;
   const letterFound = game.checkLetter(letter);
+  // console.log(letterFound);
   updateWordDisplay();
   disableElement(event.target);
 
-  updateHangmanImage();
-
   if (letterFound && game.isWordComplete()) {
-    showWinningMessage("i mean it isnt that hard to get it right, there are only 8 planets. click restart.");
+    showWinningMessage("Congratulations! You guessed the word.");
     enableElement(reloadGameButton);
     disableLetters();
   } else if (!letterFound && game.guesses === 0) {
-    showWinningMessage("lol, dead.");
+    showWinningMessage("Game Over! You ran out of guesses.");
     enableElement(reloadGameButton);
     disableLetters();
   }
   updateGuessCount();
 }
 
-
 function reloadGame() {
   location.reload();
 }
 
 function updateWordDisplay() {
-  const wordDisplay = document.getElementById("wordDisplay");
-  wordDisplay.innerHTML = 
+  wordDisplay.textContent = game.placeholders.join(" ");
 }
 
 function disableElement(element) {
